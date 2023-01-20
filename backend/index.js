e23b74b0da771e5103c440315eaf3dd146aaf345
lucasjstn <lucas7justino@gmail.com>
2023-01-20 20:25:40 -0300

    setting backend index js to start main function

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import app from "./server";
+import mongodb from "mongodb";
+import dontenv from "dotenv";
+
+async function main() {
+    dontenv.config();
+
+    const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+    const port = process.env.PORT || 8000;
+
+    try {
+        // trying connect to the db cluster
+        await client.connect();
+
+        app.listen(port, () => {
+            console.log("server is running on port:" + port);
+        });
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+main().catch(console.error);
