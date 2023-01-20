22b2e9a835a19b228dda4d79f5ceb5a5b8ed4631
lucasjstn <lucas7justino@gmail.com>
2023-01-20 20:10:53 -0300

    serverjs starter code

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import movies from "./api/movies.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("./api/v1/movies", movies);
+app.use("*", (req, res) => {
+    res.status(404).json({ error: "not found" });
+});
+
+export default app;
+// Middleware are functions that Express executes in the middle after the
+// incoming request and before the output
