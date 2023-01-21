d6e3bf1210c50c5a6812c7d200ceac6169666adf
lucasjstn <lucas7justino@gmail.com>
2023-01-21 07:39:57 -0300

    method to retrieve movies

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -14,4 +14,35 @@ export default class MoviesDAO {
             console.error(`unable to connect in MoviesDAO: ${e}`);
         }
     }
+
+    static async getMovies({
+        filters = null,
+        page = 0,
+        moviesPerPage = 20,
+    } = {}) {
+        let query;
+        if (filters) {
+            if ("title" in filters) {
+                query = { $text: { $search: filters["title"] } };
+            } else if ("rated" in filters) {
+                query = { rated: { $eq: filters["read"] } };
+            }
+        }
+
+        let cursor;
+        try {
+            cursor = await movies
+                .find(query)
+                .limit(moviesPerPage)
+                .skip(moviesPerPage * page);
+
+            const moviesList = await cursor.toArray();
+            const totalNumMovies = await movies.countDocuments(query);
+
+            return { moviesList, totalNumMovies };
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`);
+            return { moviesList: [], totalNumMovies: 0 };
+        }
+    }
 }
