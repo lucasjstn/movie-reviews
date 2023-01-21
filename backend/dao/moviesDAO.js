a04d0894331a4d2fb8da36d2970a1c50b496fe91
lucasjstn <lucas7justino@gmail.com>
2023-01-21 07:10:03 -0300

    creating data access object

---
+++ backend/dao/moviesDAO.js
@@ -0,0 +1,17 @@
+let movies;
+
+export default class MoviesDAO {
+    static async injectDB(conn) {
+        if (movies) {
+            return;
+        }
+
+        try {
+            movies = await conn
+                .db(process.env.MOVIESREVIEWS_NS)
+                .collection("movies");
+        } catch (e) {
+            console.error(`unable to connect in MoviesDAO: ${e}`);
+        }
+    }
+}
