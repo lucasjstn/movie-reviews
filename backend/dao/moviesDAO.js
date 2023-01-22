5e3a19a00e30fdc981bf49e969fdd3b828f55f70
lucasjstn <lucas7justino@gmail.com>
2023-01-21 23:59:17 -0300

    working request

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -1,14 +1,12 @@
 let movies;
-
 export default class MoviesDAO {
     static async injectDB(conn) {
         if (movies) {
             return;
         }
-
         try {
             movies = await conn
-                .db(process.env.MOVIESREVIEWS_NS)
+                .db(process.env.MOVIEREVIEWS_NS)
                 .collection("movies");
         } catch (e) {
             console.error(`unable to connect in MoviesDAO: ${e}`);
@@ -16,6 +14,7 @@ export default class MoviesDAO {
     }
 
     static async getMovies({
+        // default filter
         filters = null,
         page = 0,
         moviesPerPage = 20,
@@ -25,20 +24,17 @@ export default class MoviesDAO {
             if ("title" in filters) {
                 query = { $text: { $search: filters["title"] } };
             } else if ("rated" in filters) {
-                query = { rated: { $eq: filters["read"] } };
+                query = { rated: { $eq: filters["rated"] } };
             }
         }
-
         let cursor;
         try {
             cursor = await movies
                 .find(query)
                 .limit(moviesPerPage)
                 .skip(moviesPerPage * page);
-
             const moviesList = await cursor.toArray();
             const totalNumMovies = await movies.countDocuments(query);
-
             return { moviesList, totalNumMovies };
         } catch (e) {
             console.error(`Unable to issue find command, ${e}`);
