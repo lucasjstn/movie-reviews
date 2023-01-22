e93f26cca24118664c21506b71a62ae0aa540a8b
lucasjstn <lucas7justino@gmail.com>
2023-01-21 23:58:16 -0300

    fixing the extension of imported file

--- backend/api/movies.controller.js
+++ backend/api/movies.controller.js
@@ -1,26 +1,21 @@
-import MoviesDAO from "../dao/moviesDAO";
-
+import MoviesDAO from "../dao/moviesDAO.js";
 export default class MoviesController {
     static async apiGetMovies(req, res, next) {
         const moviesPerPage = req.query.moviesPerPage
             ? parseInt(req.query.moviesPerPage)
             : 20;
-
         const page = req.query.page ? parseInt(req.query.page) : 0;
-
         let filters = {};
         if (req.query.rated) {
             filters.rated = req.query.rated;
         } else if (req.query.title) {
             filters.title = req.query.title;
         }
-
         const { moviesList, totalNumMovies } = await MoviesDAO.getMovies({
             filters,
             page,
             moviesPerPage,
         });
-
         let response = {
             movies: moviesList,
             page: page,
@@ -28,7 +23,6 @@ export default class MoviesController {
             entries_per_page: moviesPerPage,
             total_results: totalNumMovies,
         };
-
         res.json(response);
     }
 }
