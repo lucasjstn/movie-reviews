987490f44263e7c00797fa984f90773249c29fe3
lucasjstn <lucas7justino@gmail.com>
2023-01-22 13:51:10 -0300

    creating add review DAO method

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -20,4 +20,21 @@ export default class ReviewsDAO {
             );
         }
     }
+
+    static async addReview(movieId, user, review, date) {
+        try {
+            const reviewDoc = {
+                name: user.name,
+                user_id: user._id,
+                date: date,
+                review: review,
+                movie_id: ObjectId(movieId),
+            };
+
+            return await reviews.insertOne(reviewDoc);
+        } catch (e) {
+            console.error(`unable to post review: ${e}`);
+            return { error: e };
+        }
+    }
 }
