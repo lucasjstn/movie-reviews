4b79e32d9cba2a5067dca4ee862baf024494d121
lucasjstn <lucas7justino@gmail.com>
2023-01-22 15:58:45 -0300

    creating the deleteReview DAO method

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -51,4 +51,19 @@ export default class ReviewsDAO {
             return { error: e };
         }
     }
+
+    static async deleteReview(reviewId, userId) {
+        try {
+            const deleteResponse = await reviews.deleteOne({
+                _id: ObjectId(reviewId),
+                user_id: userId,
+            });
+
+            return;
+            deleteResponse;
+        } catch (e) {
+            console.error(`unable to delete review: ${e}`);
+            return { error: e };
+        }
+    }
 }
