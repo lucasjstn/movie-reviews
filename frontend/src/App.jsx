05822ba5c7e24702b6a20370275712265cdb6a2a
lucasjstn <lucas7justino@gmail.com>
2023-01-23 18:30:31 -0300

    importing components to app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+import AddReview from './components/add-review'
+import MoviesList from './components/movies-list'
+import Movie from './components/movie'
+import Login from './components/login'
+
 function App() {
     return <div className="App">Hello World</div>;
 }
