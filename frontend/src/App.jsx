bb05f2bf6368a7baa7a7e92f1c8f7b632689dc42
lucasjstn <lucas7justino@gmail.com>
2023-01-23 18:39:23 -0300

    navbar made using bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-
-import AddReview from './components/add-review'
-import MoviesList from './components/movies-list'
-import Movie from './components/movie'
-import Login from './components/login'
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
 export default App;
