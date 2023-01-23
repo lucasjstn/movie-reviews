058ce1cf2ae0d997705843a3f0d33b323e32efdb
lucasjstn <lucas7justino@gmail.com>
2023-01-23 19:03:55 -0300

    creating navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,12 +14,20 @@ function App() {
     return (
         <div className="App">
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/">Movie Reviews</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link>
+                            <Link to={"/movies"}>Movies</Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            {user ? (
+                                <a>Logout User</a>
+                            ) : (
+                                <Link to={"/login"}>Login</Link>
+                            )}
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
