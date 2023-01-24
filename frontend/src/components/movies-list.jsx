1c61159ce6f6a8c6d3c356cf1e2149245da31823
lucasjstn <lucas7justino@gmail.com>
2023-01-24 08:33:05 -0300

    creating usestates for the request variables

--- frontend/src/components/movies-list.jsx
+++ frontend/src/components/movies-list.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
 
-function MovieList() {
-    return <div className="App">Movies List</div>;
-}
+const MoviesList = (props) => {
+    const [movies, setMovies] = useState([]);
+    const [searchTitle, setSearchTitle] = useState("");
+    const [searchRating, setSearchRating] = useState("");
+    const [ratings, setRatings] = useState(["All Ratings"]);
+};
 
 export default MovieList;
