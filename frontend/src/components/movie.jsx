0e929d6784785259cc3c350347d1f3c84ed4e573
lucasjstn <lucas7justino@gmail.com>
2023-01-24 19:10:34 -0300

    setting up single movie request

--- frontend/src/components/movie.jsx
+++ frontend/src/components/movie.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
 
-function Movie() {
-    return <div className="App">Movie</div>;
-}
+const Movie = (props) => {
+    const [movie, setMovie] = useState({
+        id: null,
+        title: "",
+        rated: "",
+        reviews: [],
+    });
 
+    const getMovie = (id) => {
+        MovieDataService.get(id)
+            .then((response) => {
+                console.log(response.data);
+                setMovie(response.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+
+    useEffect(() => {
+        getMovie(props.match.params.id);
+    }, [props.match.params.id]);
+
+    return (
+        <div></div>
+    )
+};
 export default Movie;
