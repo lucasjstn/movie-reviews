0f27c0e044c45b17bc2bcf352939c1572a7499c1
lucasjstn <lucas7justino@gmail.com>
2023-01-25 08:08:40 -0300

    login page states and setstates functions

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function Login() {
-    return <div className="App">Login</div>;
-}
+const Login = (props) => {
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
+
+    const onChangeName = (e) => {
+        const name = e.target.value;
+        setName(name);
+    };
+
+    const onChangeId = (e) => {
+        const id = e.target.value;
+        setId(id);
+    };
+
+    const login = () => {
+        props.login({ name: name, id: id });
+        props.history.push("/");
+    };
+};
 
 export default Login;
